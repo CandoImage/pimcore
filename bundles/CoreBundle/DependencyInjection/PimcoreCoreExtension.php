7b424a679b77ce6c395d30af0679ca22dd48cd8a
Stefan Scheu <stefan.scheu@cando-image.com>
2023-07-05 17:10:30 +0200

    chore: fix config parameter

--- bundles/CoreBundle/DependencyInjection/PimcoreCoreExtension.php
+++ bundles/CoreBundle/DependencyInjection/PimcoreCoreExtension.php
@@ -91,6 +91,7 @@ final class PimcoreCoreExtension extends ConfigurableExtension implements Prepen
         $container->setParameter('pimcore.maintenance.housekeeping.cleanup_profiler_files_atime_older_than', $config['maintenance']['housekeeping']['cleanup_profiler_files_atime_older_than']);
 
         $container->setParameter('pimcore.maintenance.cleanup_directory.cleanup_files_atime_older_than', $config['maintenance']['cleanup_directory']['cleanup_files_atime_older_than']);
+        $container->setParameter('pimcore.maintenance.cleanup_directory.directories', $config['maintenance']['cleanup_directory']['directories']);
 
         $container->setParameter('pimcore.documents.default_controller', $config['documents']['default_controller']);
         $container->setParameter('pimcore.documents.web_to_print.default_controller_print_page', $config['documents']['web_to_print']['default_controller_print_page']);
